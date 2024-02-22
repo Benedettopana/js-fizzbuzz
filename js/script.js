29646554b6cacfc38d09383fee596591c0eb4d01
Benedettopana <benedetto.pana@gmail.com>
2024-02-22 14:34:20 +0100

    [add]Ciclo FOR

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const fizz = 'Fizz'; //multiplo 3
+const buzz = 'Buzz'; //multiplo 5
+
+// Faccio partire il ciclo da uno e lo continuo finchè non fa 100 iterazioni
+for(let i=1; i <= 100; i++){
+  console.log(i);
+}
+
